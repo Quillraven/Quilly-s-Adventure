9509d7392ad7c255146b33e11dcc24850d5022de
Quillraven <quillraven@gmail.com>
2019-09-19 22:14:28 +0200

    - added sound and music cache to avoid retrieving the same assets multiple times from the assetmanager which also creates some unnecessary string garbage

--- core/src/main/kotlin/com/game/quillyjumper/AudioManager.kt
+++ core/src/main/kotlin/com/game/quillyjumper/AudioManager.kt
@@ -2,22 +2,26 @@ package com.game.quillyjumper
 
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Music
+import com.badlogic.gdx.audio.Sound
 import com.game.quillyjumper.assets.MusicAssets
 import com.game.quillyjumper.assets.SoundAssets
 import com.game.quillyjumper.assets.get
 import com.game.quillyjumper.map.Map
 import com.game.quillyjumper.map.MapChangeListener
+import java.util.*
 
 class AudioManager(private val assets: AssetManager) : MapChangeListener {
     private var music: Music? = null
     private var musicVolume = 1f
     private var soundVolume = 1f
+    private val soundCache = EnumMap<SoundAssets, Sound>(SoundAssets::class.java)
+    private val musicCache = EnumMap<MusicAssets, Music>(MusicAssets::class.java)
 
     fun play(type: MusicAssets) {
         // stop current music
         music?.stop()
         // play new music
-        music = assets[type].apply {
+        music = musicCache.computeIfAbsent(type) { assets[type] }.apply {
             volume = (musicVolume * type.volumeScale)
             isLooping = true
             play()
@@ -25,7 +29,7 @@ class AudioManager(private val assets: AssetManager) : MapChangeListener {
     }
 
     fun play(type: SoundAssets) {
-        assets[type].play(soundVolume * type.volumeScale)
+        soundCache.computeIfAbsent(type) { assets[type] }.play(soundVolume * type.volumeScale)
     }
 
     override fun mapChange(newMap: Map) {
