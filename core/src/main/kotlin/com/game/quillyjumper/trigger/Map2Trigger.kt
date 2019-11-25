2f016ecab71b3b042822abae8ab90536112708ec
Quillraven <quillraven@gmail.com>
2019-11-25 21:24:03 +0100

    - enhanced the trigger class to now also be able to spawn characters and to enable/disable portals - map2 boss trigger is now spawning the boss and also activates the rescue portal once the boss is killed

--- core/src/main/kotlin/com/game/quillyjumper/trigger/Map2Trigger.kt
+++ core/src/main/kotlin/com/game/quillyjumper/trigger/Map2Trigger.kt
@@ -2,53 +2,92 @@ package com.game.quillyjumper.trigger
 
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.physics.box2d.World
 import com.game.quillyjumper.AudioManager
 import com.game.quillyjumper.assets.MusicAssets
-import com.game.quillyjumper.ecs.component.ModelType
-import com.game.quillyjumper.ecs.component.aniCmp
+import com.game.quillyjumper.assets.ParticleAssets
+import com.game.quillyjumper.configuration.Character
+import com.game.quillyjumper.configuration.CharacterConfigurations
+import com.game.quillyjumper.ecs.character
+import com.game.quillyjumper.ecs.component.FadeInComponent
+import com.game.quillyjumper.ecs.component.ParticleComponent
+import com.game.quillyjumper.ecs.component.portalCmp
+import com.game.quillyjumper.ecs.findPortal
 import com.game.quillyjumper.event.GameEventListener
 import com.game.quillyjumper.event.GameEventManager
 
 class Map2TriggerEnter(
+    triggerEntity: Entity,
     gameEventManager: GameEventManager,
     audioManager: AudioManager,
     engine: Engine,
-    world: World
-) : Trigger(gameEventManager, audioManager, engine, world) {
+    world: World,
+    characterCfgs: CharacterConfigurations
+) : Trigger(triggerEntity, gameEventManager, audioManager, engine, world, characterCfgs) {
     init {
         // TODO show cave dialog
         println("Entering cave")
+        destroy()
     }
 }
 
 class Map2TriggerBoss(
+    triggerEntity: Entity,
     gameEventManager: GameEventManager,
     audioManager: AudioManager,
     engine: Engine,
-    world: World
-) : Trigger(gameEventManager, audioManager, engine, world), GameEventListener {
-    private var isMinotaurDead = false
-    private var isSkeletalDead = false
+    world: World,
+    characterCfgs: CharacterConfigurations
+) : Trigger(triggerEntity, gameEventManager, audioManager, engine, world, characterCfgs), GameEventListener,
+    Music.OnCompletionListener {
+    private lateinit var minotaur: Entity
+    private lateinit var skeletal: Entity
+    private var killCounter = -1
 
     init {
         gameEventManager.addGameEventListener(this)
     }
 
     override fun playerTriggerContact(player: Entity, trigger: Entity) {
+        if (trigger != triggerEntity) return
+
         audioManager.play(MusicAssets.BOSS_1)
+        killCounter = 0
+        minotaur = engine.character(
+            characterCfgs[Character.MINOTAUR],
+            world,
+            8f,
+            2.5f
+        ) { with<FadeInComponent> { maxFadeTime = 2f } }
+        skeletal = engine.character(
+            characterCfgs[Character.SKELETAL],
+            world,
+            8f,
+            2.3f
+        ) { with<FadeInComponent> { maxFadeTime = 2f } }
     }
 
     override fun characterDeath(character: Entity) {
-        when (character.aniCmp.modelType) {
-            ModelType.MINOTAUR -> isMinotaurDead = true
-            ModelType.SKELETAL -> isSkeletalDead = true
-            else -> {
+        if (killCounter >= 0 && (character == minotaur || character == skeletal)) {
+            ++killCounter
+            if (killCounter >= 2) {
+                engine.findPortal(97) { portal ->
+                    portal.portalCmp.active = true
+                    portal.add(engine.createComponent(ParticleComponent::class.java).apply {
+                        type = ParticleAssets.PORTAL
+                    })
+                }
+                gameEventManager.disablePlayerInput()
+                audioManager.play(MusicAssets.FANFARE, false).setOnCompletionListener(this)
             }
         }
+    }
 
-        if (isMinotaurDead && isSkeletalDead) {
-            audioManager.play(MusicAssets.FANFARE)
-        }
+    override fun onCompletion(music: Music) {
+        // fanfare finished -> create portal to port back to top of cave and do amazing stuff
+        gameEventManager.enablePlayerInput()
+        audioManager.play(MusicAssets.LEVEL_2)
+        destroy()
     }
 }
--- core/src/main/kotlin/com/game/quillyjumper/trigger/Trigger.kt
+++ core/src/main/kotlin/com/game/quillyjumper/trigger/Trigger.kt
@@ -1,17 +1,26 @@
 package com.game.quillyjumper.trigger
 
 import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.physics.box2d.World
 import com.game.quillyjumper.AudioManager
+import com.game.quillyjumper.configuration.CharacterConfigurations
+import com.game.quillyjumper.ecs.component.RemoveComponent
 import com.game.quillyjumper.event.GameEventManager
 
 abstract class Trigger(
+    val triggerEntity: Entity,
     val gameEventManager: GameEventManager,
     val audioManager: AudioManager,
     val engine: Engine,
-    val world: World
+    val world: World,
+    val characterCfgs: CharacterConfigurations
 ) {
     fun destroy() {
+        triggerEntity.add(engine.createComponent(RemoveComponent::class.java))
+    }
+
+    fun cleanup() {
         gameEventManager.removeFromAllListeners(this)
     }
 }
