8c90f3e07b4a3f917cca158f52f60546106b0db4
Quillraven <quillraven@gmail.com>
2019-11-25 21:19:46 +0100

    new fadein component to fadein sprites smoothly (like e.g. spawning enemies)

---
+++ core/src/main/kotlin/com/game/quillyjumper/ecs/component/FadeInComponent.kt
@@ -0,0 +1,31 @@
+package com.game.quillyjumper.ecs.component
+
+import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.utils.Pool
+import ktx.ashley.get
+import ktx.ashley.mapperFor
+
+class FadeInComponent(
+    var targetAlpha: Float = 1f,
+    var startAlpha: Float = 0f,
+    var fadeTime: Float = 0f,
+    var maxFadeTime: Float = 1f
+) : Component,
+    Pool.Poolable {
+    companion object {
+        val mapper = mapperFor<FadeInComponent>()
+    }
+
+    override fun reset() {
+        startAlpha = 0f
+        targetAlpha = 1f
+        fadeTime = 0f
+        maxFadeTime = 1f
+    }
+}
+
+
+val Entity.fadeinCmp: FadeInComponent
+    get() = this[FadeInComponent.mapper]
+        ?: throw KotlinNullPointerException("Trying to access a fadein component which is null")
