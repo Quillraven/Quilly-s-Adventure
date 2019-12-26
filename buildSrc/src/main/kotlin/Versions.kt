e3e94f386060a0a40dd4b70f0b6bfaa2663bf88a
Michael Rittmeister <michael@rittmeister.in>
2019-12-26 23:32:13 +0100

    Add Versions.kt again

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    const val androidGradlePlugin = "3.5.3"
+    const val kotlin = "1.3.61"
+    const val gdx = "1.9.10"
+    const val ktx = "1.9.10-b2"
+    const val box2DLight = "1.4"
+    const val ashley = "1.7.3"
+    const val gdxAI = "1.8.2"
+
+    const val jvmTarget = "1.8"
+    const val packageName = "com.game.quillyjumper"
+
+    // version of the game
+    const val version = "1.0"
+    const val androidVersionCode = 1
+}
