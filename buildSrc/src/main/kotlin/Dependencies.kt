4c688fe4797502f5c8992a947d29cc1954c09b9c
Simon <quillraven@gmail.com>
2020-04-11 12:07:57 +0200

    update libktx to stable 1.9.10-b5 version instead of snapshot (#42)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
     const val gdx = "1.9.10"
-    const val ktx = "1.9.10-SNAPSHOT"
+    const val ktx = "1.9.10-b5"
     const val box2DLight = "1.4"
     const val ashley = "1.7.3"
     const val gdxAI = "1.8.2"
@@ -21,8 +21,8 @@ object Apps {
     const val compileSdk = 29
     const val minSdk = 24
     const val targetSdk = 29
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
     const val packageName = "com.github.quillraven.quillysadventure"
     const val name = "Quilly's Adventure"
 }
