c37946f57d8e15787b5b9d905fb94fa838107ed3
Quillraven <quillraven@gmail.com>
2020-06-07 13:40:08 +0200

    fix a bug that entities are not sorted correctly for rendering

--- core/src/main/kotlin/com/github/quillraven/quillysadventure/ecs/component/TransformComponent.kt
+++ core/src/main/kotlin/com/github/quillraven/quillysadventure/ecs/component/TransformComponent.kt
@@ -7,7 +7,6 @@ import com.badlogic.gdx.utils.Pool
 import ktx.ashley.get
 import ktx.ashley.mapperFor
 import ktx.math.vec2
-import kotlin.math.roundToInt
 
 class TransformComponent(
     var position: Vector2 = vec2(0f, 0f),
@@ -19,8 +18,8 @@ class TransformComponent(
     // entities are sorted first by their z index (background/foreground) and then by their
     // y-coordinate of their position on the current layer (=z index)
     override fun compareTo(other: TransformComponent): Int {
-        val zDiff = z - other.z
-        return if (zDiff == 0) (position.y - other.position.y).roundToInt() else zDiff
+        val zDiff = other.z.compareTo(z)
+        return if (zDiff == 0) other.position.y.compareTo(position.y) else zDiff
     }
 
     companion object {
